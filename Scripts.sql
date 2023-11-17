1f21960e61078a5e0af57bd5fe3c4c1915b9971d
Marcella Rappoli Bedinelli <105222261+marappoli@users.noreply.github.com>
2023-11-16 21:43:24 -0300

    Update Scripts.sql

--- Scripts.sql
+++ Scripts.sql
@@ -1,4 +1,4 @@
-/*Primeira parte do Projeto. Banco escolhido: wide-colum storage*/
+/*Primeira parte do Projeto*/
 
 --keyspace no Cassandra
 CREATE KEYSPACE university_keyspace
@@ -87,10 +87,39 @@ CREATE TABLE advisor (
     i_ID INT
 );
 
---armazena os dados agregados para cada departamento
-CREATE TABLE department_summary (
-    dept_name TEXT PRIMARY KEY,
-    budget FLOAT,
-    total_students INT,
-    total_salary FLOAT
-);
+--inserindo na tabela department
+INSERT INTO department (dept_name, building, budget) VALUES ('CS', 'Turing Hall', 1000000.0);
+INSERT INTO department (dept_name, building, budget) VALUES ('Math', 'Einstein Building', 800000.0);
+
+--inserindo na tabela instructor
+INSERT INTO instructor (ID, name, dept_name, salary) VALUES (101, 'John Smith', 'CS', 80000.0);
+INSERT INTO instructor (ID, name, dept_name, salary) VALUES (102, 'Jane Doe', 'Math', 75000.0);
+
+--inserindo na tabela student
+INSERT INTO student (ID, name, dept_name, tot_cred) VALUES (201, 'Alice Johnson', 'CS', 120);
+INSERT INTO student (ID, name, dept_name, tot_cred) VALUES (202, 'Bob Davis', 'Math', 110);
+
+--inserindo na tabela course
+INSERT INTO course (course_id, title, dept_name, credits) VALUES ('CS101', 'Introduction to Computer Science', 'CS', 3);
+INSERT INTO course (course_id, title, dept_name, credits) VALUES ('MATH101', 'Introduction to Mathematics', 'Math', 3);
+
+
+--inserindo na tabela section
+INSERT INTO section (course_id, sec_id, semester, year, building, room_number, time_slot_id) VALUES ('CS101', '001', 'Fall', 2023, 'Turing Hall', '101', '1-1-1');
+INSERT INTO section (course_id, sec_id, semester, year, building, room_number, time_slot_id) VALUES ('MATH101', '001', 'Fall', 2023, 'Einstein Building', '201', '1-1-2');
+
+--inserindo na tabela time_slot
+INSERT INTO time_slot (time_slot_id, day, start_hr, start_min, end_hr, end_min) VALUES ('1-1-1', 'Monday', 8, 0, 9, 30);
+INSERT INTO time_slot (time_slot_id, day, start_hr, start_min, end_hr, end_min) VALUES ('1-1-2', 'Monday', 10, 0, 11, 30);
+
+--inserindo na tabela teaches
+INSERT INTO teaches (ID, course_id, sec_id, semester, year) VALUES (101, 'CS101', '001', 'Fall', 2023);
+INSERT INTO teaches (ID, course_id, sec_id, semester, year) VALUES (102, 'MATH101', '001', 'Fall', 2023);
+
+--inserindo na tabela takes
+INSERT INTO takes (ID, course_id, sec_id, semester, year, grade) VALUES (201, 'CS101', '001', 'Fall', 2023, 'A');
+INSERT INTO takes (ID, course_id, sec_id, semester, year, grade) VALUES (202, 'MATH101', '001', 'Fall', 2023, 'B');
+
+--inserindo na tabela advisor
+INSERT INTO advisor (s_ID, i_ID) VALUES (201, 101);
+INSERT INTO advisor (s_ID, i_ID) VALUES (202, 102);
